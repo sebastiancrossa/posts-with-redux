b08fb704c26bee13a3047e41bad54591afaaba4b
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-17 16:26:29 -0600

    Finished off the basics for the form and posts components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 // COMPONENT IMPORTS
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
+          <PostForm />
+          <hr />
           <Posts />
         </header>
       </div>
